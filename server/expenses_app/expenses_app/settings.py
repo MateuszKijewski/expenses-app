58325822a415fbacd98cad642475443c6f8128ae
Mateusz <mateuszkijewski2307@gmail.com>
2019-10-19 17:45:31 +0200

    Finished user configuration

--- server/expenses_app/expenses_app/settings.py
+++ server/expenses_app/expenses_app/settings.py
@@ -18,21 +18,22 @@ from django.core.exceptions import ImproperlyConfigured
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Passwords configuration
-with open(os.path.join(BASE_DIR, '../../secrets.json')) as secrets_file:
-    secrets = json.load(secrets_file)
+# with open(os.path.join(BASE_DIR, '../../secrets.json')) as secrets_file:
+#     secrets = json.load(secrets_file)
 
-def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting))
+# def get_secret(setting, secrets=secrets):
+#     """Get secret setting or fail with ImproperlyConfigured"""
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         raise ImproperlyConfigured("Set the {} setting".format(setting))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret('SECRET_KEY')
+SECRET_KEY = 'kp#fup5iev55o4foz0(b7vuu+*!5o6raiu_75lcj$38#i+6q53'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
