7764e8b9d1c11d8a48a92ed71a8ed33d70be4e47
Mateusz <mateuszkijewski2307@gmail.com>
2019-11-25 23:01:05 +0100

    Connected MySQL database

--- server/expenses_app/expenses_app/settings.py
+++ server/expenses_app/expenses_app/settings.py
@@ -98,10 +98,21 @@ WSGI_APPLICATION = 'expenses_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cbuP3MtOw7',
+        'USER': 'cbuP3MtOw7',
+        'PASSWORD': 'wh9Ro9dVB2',
+        'HOST': 'remotemysql.com',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
